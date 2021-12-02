efaae6c494fca50d95d99f4eabb5a1181d71ea49
thebreezyanupam <theanupamofficial@gmail.com>
2021-12-02 09:42:53 +0545

    LabWork

---
+++ LabWork2.py
@@ -0,0 +1,5 @@
+#SUM OF INTEGERS
+num=int(input("Enter your number"))
+num1=int(input("Enter your second number"))
+num2=int(input("enter your third number"))
+print("The sum of given variables is :",num+num1+num2)
