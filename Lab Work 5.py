f2e7e2eb4a335d00b5ea4dd6c0afff0b7e96932e
thebreezyanupam <theanupamofficial@gmail.com>
2021-12-02 09:43:50 +0545

    Lab Work

---
+++ Lab Work 5.py
@@ -0,0 +1,3 @@
+#SUM OF FIRST 10 INTEGERS
+list=[1,2,3,4,5,6,7,8,9,10]
+print(sum(list))
---
+++ LabWork3.py
@@ -0,0 +1,4 @@
+#VOLUME OF CUBE
+l=int(input("Enter the length"))
+V=(l*l*l)
+print("The volume of a cube is {} cm\u00b3".format(V))
